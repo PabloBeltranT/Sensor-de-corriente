b3391e99a38d96f5016b788c5206d2cc004669c0
pablo beltran <pablobeltran219@gmail.com>
2021-05-19 09:44:15 -0700

    actualice firmware para arduino

--- sketch_may05a/sketch_may05a.ino
+++ sketch_may05a/sketch_may05a.ino
@@ -1,27 +1,48 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-
+  pinMode(8, OUTPUT), pinMode(9, OUTPUT), pinMode(13, OUTPUT);
 }
 
-void loop() {
-  if (Serial.available() > 0){
-    char estado = Serial.read();
 
-    switch(estado){
-      case 'a':
-      digitalWrite(13, HIGH);
-      break;
-      case 'b':
+void imprimir(String mensaje, int cajas){
+  Serial.print(mensaje), Serial.println(cajas);
+}
+
+float entrada, 
+      voltaje, 
+      resistencia = 0.9, 
+      corriente;
+
+void lectura(){
+  
+  float muestra=0;
+  for(int i=0; i<=50; i++){
+    entrada = analogRead(A1);
+    voltaje = entrada * 6 / 1024;
+    corriente = voltaje / resistencia;
+    muestra = muestra + voltaje;
+  }
+  muestra = (muestra / 50) * 2;
+  Serial.println(muestra, 4);
+}
+
+
+void loop() {
+
+  char in = Serial.read();
+
+  switch(in){
+    case 'a':
+      lectura();
+    break;
+
+    case 'b':
       digitalWrite(13, LOW);
-      break;
-      case 'c':
-      while(true){
-        int voltaje = analogRead(A0);
-        Serial.println(voltaje); 
-      }
-      break;
-    }
+    break;
+
+    case 'c':
+      digitalWrite(13, HIGH);
+    break;
   }
 
 }
